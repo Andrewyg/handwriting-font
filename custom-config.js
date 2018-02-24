99c84b561070dc731e3d5f42a341f170f1852aab
Andrew <jiganyg@gmail.com>
2018-02-24 23:10:32 +0800

    Add files via upload

---
+++ custom-config.js
@@ -0,0 +1,6 @@
+CKEDITOR.editorConfig = function(config) {
+    config.contentsCss = './custom-font.css';
+    config.font_names = 'Handwrite (DEFAULT)/SentyTEA' + config.font_names;
+    config.font_defaultLabel = 'SentyTEA';
+    config.fontSize_defaultLabel = '24px';
+}
